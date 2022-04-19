7780931bf9cbc9b34c7901604631c0c92294e499
Salih <94194901+salihburock@users.noreply.github.com>
2022-04-19 12:06:20 +0300

    Files modified

--- windowed.py
+++ windowed.py
@@ -5,10 +5,52 @@ root = Tk()
 root.title("Son Harften Kelime Türetme")
 
 def kelime_gonder():
-    pass
-    donut.set("Po")
+    kelime = kelime_bul(giris.get())
+    donut.set(kelime_bul(kelime))
 def kelime_bul(kelime):
-    pass
+    mümkün_kelimeler = []
+
+    ilkkelime = input('Kelime giriniz:').lower()
+    sonharf = ilkkelime[-1]
+    if kelimeler.count(ilkkelime.lower()) == 0:
+        print(ilkkelime, 'diye bir kelime bulunamadı')
+        oyun_bitti()
+        exit()
+    if ilkkelime.endswith('ğ'):
+        print('Sen kazandın!')
+        oyun_bitti()
+        exit()
+    for kelime in kelimeler:
+        if kelime.startswith(sonharf) and kullanılan_kelimeler.count(kelime) == 0:
+            mümkün_kelimeler.append(kelime)
+
+    dönüt = np.random.choice(mümkün_kelimeler)
+    try:
+        global stop
+        if not ilkkelime.startswith(dönütler[-1][-1]):
+            print(f'{dönütler[-1][-1]} harfi ile başlayan bir kelime bulmanız gerekiyordu')
+            oyun_bitti()
+            os.system('exit')
+            os.system('taskkill /f /im python.exe')
+    except:
+        pass
+
+    if kullanılan_kelimeler.count(ilkkelime) != 0:
+        print('Bu kelime kullanıldı!')
+        kullanılan_kelimeler.append(dönüt)
+        kullanılan_kelimeler.append(ilkkelime)
+        skor += 1
+        print(dönütler[-1])
+
+    else:
+        print(dönüt)
+        kullanılan_kelimeler.append(dönüt)
+        kullanılan_kelimeler.append(ilkkelime)
+        skor += 1
+
+    dönütler.append(dönüt)
+    return dönüt
+
 
 mainframe = ttk.Frame(root, padding="6 6 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
