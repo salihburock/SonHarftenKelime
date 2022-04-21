510b70416552a9e03a36b41c6e0470142b74dc49
Salih <94194901+salihburock@users.noreply.github.com>
2022-04-21 11:49:32 +0300

    Update sonhal.py

--- sonhal.py
+++ sonhal.py
@@ -1,16 +1,41 @@
 from tkinter import *
+import sys
 from tkinter import ttk
 import tkinter as tk
 import random
 from time import sleep
 import os
+import subprocess as sp
+pd = ""
+try:
+    pd = sys.argv[1]
+except:
+    pass
 od=0
+
+if pd != "yo":
+    giris_ekranı = tk.Tk()
+    giris_ekranı.title("Son Harften Kelime Türetme")
+    giris_ekranı.geometry("300x200")
+    giris_ekranı.configure(bg="#ab6700")
+    linklabel= tk.Label(text="")
+    girislabeli = tk.Label(text="Son Harften Kelime Türetme Oyunu",bg="#ab6700",fg="#130675",font=("Comic Sans MS",12,"bold"))
+    girislabeli.pack()
+    kapatbutonu = tk.Button(text="    Çıkış    ",fg="red",command=exit,bg="black",pady=20)
+    ileributonu = tk.Button(text="Oyuna Başla",fg="lime",command=giris_ekranı.destroy,bg="black",pady=20)
+    kapatbutonu.pack(side="left")
+    ileributonu.pack(side="right")
+    giris_ekranı.resizable(0,0)
+    giris_ekranı.iconbitmap("icon.ico")
+    giris_ekranı.protocol("WM_DELETE_WINDOW",exit)
+    giris_ekranı.mainloop()
+
 kelimeler = []
-for kelime in open("full.txt","r+",encoding="utf-8").read().split("\n"):
+for kelime in open("liste1.txt","r+",encoding="utf-8").read().split("\n"):
     kelimeler.append(kelime.split(' ')[0])
 def yeniden_oyna():
-    os.system(f"python {__file__}")
-    root.destroy()
+    sp.run("baslat.bat")
+    root.quit()    
 bilgisayar_kelimesi = random.choice(kelimeler)
 def oyun_bitti(skor,durum=bool):
     global od
@@ -32,9 +57,9 @@ def oyun_bitti(skor,durum=bool):
         """)
     donat.config(text="")
     gönder_buton.config(state="disabled")
-    yenidenoynalabeli.place(x=50,y=70)
-    yenidenoynabutonu.place(x=100,y=90)
-    yenidenoynayıiptalbutonu.place(x=50,y=90)
+    yenidenoynalabeli.place(x=318,y=70)
+    yenidenoynabutonu.place(x=390,y=90)
+    yenidenoynayıiptalbutonu.place(x=309,y=90)
 
 kullanılan_kelimeler = []
 mümkün_kelimeler = []
@@ -42,9 +67,12 @@ skor = 0
 root = Tk()
 root.title("Son Harften Kelime Türetme")
 root.geometry("721x400")
+root.resizable(0, 0)
+root.iconbitmap("icon.ico")
+
 
 def bilgisayarın_kelime_bulması(kelime):
-    global skor, mümkün_kelimeler, bilgisayar_kelimesi
+    global skor, mümkün_kelimeler, bilgisayar_kelimesi, od
     kullanılan_kelimeler.append(kelime)
     for mkelime in kelimeler:
         if mkelime.startswith(kelime[-1]) and kullanılan_kelimeler.count(mkelime) == 0:
@@ -57,14 +85,14 @@ def bilgisayarın_kelime_bulması(kelime):
 
 def bizim_kelime_göndermemiz():
 
-    global bilgisayar_kelimesi,skor,mümkün_kelimeler
+    global bilgisayar_kelimesi,skor,mümkün_kelimeler, od
     kullanıcıKelimesi = giris_entry.get()
     skor += 1
     donat.config(text=bilgisayar_kelimesi)
     kullanılan_kelimeler.append(bilgisayar_kelimesi)
     
     if kelimeler.count(kullanıcıKelimesi) == 0:
-        hatamesajılabeli.config(text=f'{hatamesajılabeli.cget("text")}\n{kullanıcıKelimesi}, "diye bir kelime bulunamadı!"')
+        hatamesajılabeli.config(text=f'{hatamesajılabeli.cget("text")}\n{kullanıcıKelimesi}, diye bir kelime bulunamadı!')
         oyun_bitti(skor,0)
         od=1
     if not kullanıcıKelimesi.startswith(bilgisayar_kelimesi[-1]):
@@ -84,7 +112,8 @@ def bizim_kelime_göndermemiz():
         od=1
     mümkün_kelimeler = []
     donat.config(text="")
-    
+    skorlabel.config(text=f"Skor: {skor-1}")
+
     bilgisayarın_kelime_bulması(kelime=kullanıcıKelimesi)
     if od:
         donat.config(text="")
@@ -93,6 +122,8 @@ def bizim_kelime_göndermemiz():
 kullanıcı_giriş_paneli = ttk.Entry()
 noidea = tk.Label(font=("Arial",15),pady=3,padx=4,text=f"{bilgisayar_kelimesi[-1]} ile başlayan bir kelime giriniz: ")
 noidea.pack(side="top")
+skorlabel = tk.Label(text="Skor:",fg="lightblue")
+skorlabel.place(x=339,y=50)
 giris_entry = tk.Entry(fg="#fff",bg="black")
 giris_entry.pack(side="top")
 çıkış_butonu = tk.Button(text="       Çıkış       ",command=exit,bg="gray",fg="red")
@@ -109,4 +140,6 @@ yenidenoynayıiptalbutonu = tk.Button(text="✖",bg="red",command=exit)
 hatamesajılabeli = tk.Label(fg="red",text="")
 hatamesajılabeli.pack(side="bottom")
 giris_entry.focus()
+root.attributes("-topmost", True)
+
 root.mainloop()
